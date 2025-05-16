6b12d68bd931d04e3b39ee2c042f8a1c5abca84d
AikiOno <241016@std.hi-joho.ac.jp>
2025-05-16 11:55:23 +0900

    小野愛希

---
+++ src/Review1.java
@@ -0,0 +1,10 @@
+public class Review1 {
+    public static void main(String[] args) {
+        System.out.print("こんにちは。");
+        System.out.println("今日の天気は"+"   "+"  晴れです。");
+        System.out.println("今日は一日");
+        System.out.print("洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.println("文字は"+",文字列は”で囲みましょう" );
+    }
+}
