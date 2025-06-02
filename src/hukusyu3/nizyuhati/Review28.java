b8a93b6074f384496a77f03426d612a22817d482
AikiOno <241016@std.hi-joho.ac.jp>
2025-06-02 11:30:22 +0900

    小野愛希

--- src/hukusyu3/nizyuhati/Review28.java
+++ src/hukusyu3/nizyuhati/Review28.java
@@ -1,20 +1,20 @@
 package hukusyu3.nizyuhati;
 
 import java.io.*;
-public class Review29 {
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("宛先を入力>");
-        String to = br.readLine();
-        System.out.print("件名を入力>");
-        String subject = br.readLine();
-        System.out.print("本文を入力>");
-        String body = br.readLine();
-        Email email=new Email();
-        if (subject.isEmpty()) {
-            email.send(to, body);
-        }else{
-            email.send(to, subject, body);
-        }
+public class Review28 {
+    public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("宛先を入力>");
+            String to = br.readLine();
+            System.out.print("件名を入力>");
+            String subject = br.readLine();
+            System.out.print("本文を入力>");
+            String body = br.readLine();
+            Email email=new Email(); // インスタンス生成
+            if (subject.isEmpty()) { // 件名が空かどうかで分岐
+                email.send(to, body);
+                } else {
+                email.send(subject, body);
+            }
     }
-}
+ }
--- src/hukusyu3/nizyukyu/Review29.java
+++ src/hukusyu3/nizyukyu/Review29.java
@@ -1,22 +1,16 @@
 package hukusyu3.nizyukyu;
 
-import java.util.Scanner;
-
 public class Review29 {
-    public class Main {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("宛先を入力>");
-            String to =sc.nextLine() ;
-            System.out.print("件名を入力>");
-            String subject =sc.nextLine() ;
-            System.out.print("本文を入力>");
-            String body =sc.nextLine() ;
-            System.out.println(to + "に以下のメールを送信しました。");
-            System.out.println("件名: " + subject);
-            System.out.println("本文: " + body);
-        }
+    public static void main(String[] args) {
+        KeyInput key = new KeyInput();
+        System.out.print("宛先を入力>");
+        String to = key.readString();
+        System.out.print("件名を入力>");
+        String subject = key.readString();
+        System.out.print("本文を入力>");
+        String body = key.readString();
+        System.out.println(to + "に以下のメールを送信しました。");
+        System.out.println("件名: " + subject);
+        System.out.println("本文: " + body);
     }
 }
-
-
