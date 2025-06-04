bed40def7f076e4b5274d3dd2d8bf9ccf75d8316
AikiOno <241016@std.hi-joho.ac.jp>
2025-06-04 09:48:55 +0900

    小野愛希

--- src/chapterwan/itinosan.java
+++ src/chapterwan/itinosan.java
@@ -2,6 +2,10 @@ package chapterwan;
 
 public class itinosan {
     public static void main(String[] args) {
-
+        boolean flg=true;
+        System.out.print("'駆'");
+        double PI= 3.14;
+        long a=314159265853979L;
+        System.out.println("ミナトの攻撃！敵に15ポイントのダメージを与えた。");
     }
 }
